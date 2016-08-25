dea2dd7753e30f0598aa227bb7b3eba309697791
Erick Gallani <erick.gallani.1@gmail.com>
2016-08-25 00:15:55 -0300

    Iniciando tela de login

---
+++ iwashcar/www/js/app/config/main.js
@@ -0,0 +1,6 @@
+angular.module('controllers', []);
+angular.module('directives', []);
+angular.module('services', []);
+angular.module('factories', []);
+angular.module('app', ['ionic', 'angular-ladda', 'ui.router', 'ngMaterial', 'ngResource', 'ngMessages', 'ngStorage',
+                       'controllers', 'directives', 'services', 'factories']);
